3565a4ddb3048e8ed6076b40dbfb8b83a326b420
Hireki <imhireki@gmail.com>
2021-04-14 13:18:31 -0300

    add ListaProdutos and DetalheProduto content

--- produto/views.py
+++ produto/views.py
@@ -1,14 +1,20 @@
-from django.views.generic import View
+from django.views.generic import View, ListView, DetailView
 from django.http import HttpResponse
+from . import models
 
 
-class ListaProdutos(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Lista')
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 3
 
 
-class DetalheProduto(View):
-    pass
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
