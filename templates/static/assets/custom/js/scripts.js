046d1bc2812b0307d389c927f4ad933270e6b3de
Hireki <imhireki@gmail.com>
2021-04-16 16:20:38 -0300

    update JS

--- templates/static/assets/custom/js/scripts.js
+++ templates/static/assets/custom/js/scripts.js
@@ -1,25 +1,42 @@
 (function () {
+    // get select element
     select_variacao = document.getElementById('select-variacoes');
+
+    // get precos *those who are already being showed*
     variation_preco = document.getElementById('variation-preco');
     variation_preco_promocional = document.getElementById('variation-preco-promocional');
 
+    // return if select_variacao not 've clicked yet
     if (!select_variacao) {
         return;
     }
 
+    // return if variacao_preco is absent
     if (!variation_preco) {
         return;
     }
 
+    // add event listener 4 change precos, based on those from variacao
     select_variacao.addEventListener('change', function () {
+
+        //  get variacao precos from select options 
         preco = this.options[this.selectedIndex].getAttribute('data-preco');
         preco_promocional = this.options[this.selectedIndex].getAttribute('data-preco-promocional');
 
-        variation_preco.innerHTML = preco;
-
-        if (variation_preco_promocional) {
-            variation_preco_promocional.innerHTML = preco_promocional;
+        // set values
+        if (variation_preco && preco) {
+            variation_preco.innerHTML = preco;
         }
+        
+        if (variation_preco_promocional && preco_promocional) {
+            variation_preco_promocional
+        
+        } else {
+            variation_preco_promocional = preco;
+            variation_preco.innerHTML = '';
+ 
+        }
+
     })
 })();
 
