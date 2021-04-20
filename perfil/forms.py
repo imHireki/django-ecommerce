0ba29026a9b90e0ac810c3b3e690d686d90e2365
Hireki <imhireki@gmail.com>
2021-04-19 21:51:19 -0300

    add forms and views from perfil

---
+++ perfil/forms.py
@@ -0,0 +1,82 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+
+class PerfilForm(forms.ModelForm):
+    class Meta:
+        model = models.Perfil
+        field = '__all__'
+        exclude = ('usuario', )
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(),
+        label='Senha'
+    )
+    password2 = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(),
+        label='Confirmação Senha'
+    )
+
+    def __init__(self, usuario=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.usuario = usuario
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'username', 'password',
+            'password2', 'email'
+        )
+    
+    def clean(self, *args, **kwargs):
+        data = self.data
+        cleaned = self.cleaned_data
+        validation_error_msgs = {}
+
+        usuario_data = cleaned.get('username')
+        email_data = cleaned.get('email')
+        password_data = cleaned.get('password')
+        password2_data = cleaned.get('password2')
+
+        usuario_db = User.objects.filter(username=usuario_data).first()
+        email_db = User.objects.filter(email=email_data).first()
+
+        error_msg_user_exists = 'Usuário já existe'
+        error_msg_email_exists = 'E-mail já existe'
+        error_msg_password_match = 'As duas senhas não conferem'
+        error_msg_password_short = 'Sua senha precisa de pelo menos 6 caracteres'
+        error_msg_required_field = 'Este campo é obrigatório'
+
+        #  user logged in
+        if self.usuario:
+
+            # error if a logged user is trying to define a username that already is on db
+            if usuario_db: # if any profile exists with the sent username
+                if self.usuario.username != usuario_db.username: # if logged username != queried username
+                    validation_error_msgs['username'] =  error_msg_user_exists
+
+            # error if a logged user is trying to define a email that already is on db
+            if email_db: # if any profile exists with the sent username
+                if self.usuario.email != email_db.email: # if logged user's email != queried email
+                    validation_error_msgs['email'] = error_msg_email_exists
+
+            if password_data:
+                if password_data != password2_data:
+                    validation_error_msgs['password'] = error_msg_password_match
+                    validation_error_msgs['password2'] = error_msg_password_match
+            
+                if len(password_data) < 6:
+                    validation_error_msgs['password'] =  error_msg_password_short
+
+        # user not logged in
+        else:
+            pass
+        
+        # it raises all error msgs at once
+        if validation_error_msgs:
+            raise(forms.ValidationError(validation_error_msgs))
--- perfil/views.py
+++ perfil/views.py
@@ -1,10 +1,48 @@
 from django.http import HttpResponse
 from django.views.generic import View
+from django.shortcuts import render
+from . import forms
 
 
-class Criar(View):
+class BasePerfil(View):
+    template_name = 'perfil/criar.html'
+
+    def setup(self, *args, **kwargs):
+        super().seteup(*args, **kwargs)
+
+        if self.request.user.is_authenticated:
+            self.context = {
+                'userform': forms.Userform(
+                    data=self.request.POST or None,
+                    usuario=self.request.user,
+                    instance=self.request.user
+                ),
+                'perfilform': forms.PerfilForm(
+                    data=self.request.POST or None,
+                    instance=self.perfil
+                )
+            }
+        else:
+            self.context = {
+                'userform': forms.UserForm(
+                    data=self.request.POST or None,
+                ),
+                'perfilform': forms.PerfilForm(
+                    data=self.request.POST or None,
+                )
+            }
+
+        self.renderizar = render(
+            self.request, self.template_name, self.context
+        )
+
     def get(self, *args, **kwargs):
-        return HttpResponse('Criar')
+        return self.renderizar
+    
+
+class Criar(BasePerfil):
+    def post(self, *args, **kwargs):
+        return self.renderizar
 
 
 class Atualizar(View):
