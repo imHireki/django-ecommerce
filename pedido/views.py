5afd636fdd47402c594742ed9d1d4a5e3a60a748
Hireki <imhireki@gmail.com>
2021-04-30 20:26:40 -0300

    updates SalvarPedido

--- pedido/views.py
+++ pedido/views.py
@@ -13,40 +13,71 @@ class Pagar(View):
 
 
 class SalvarPedido(View):
+    # Gets here when click on "realizar pedido e pagar" in resumodacompra.html
     def get(self, *args, **kwargs):
-        if not self.request.user.is_authenticated:
-            pass
-            
-        if not self.request.session.get('carrinho'):
-            pass
 
+        # it's not logged in
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                'Você precisa fazer login'
+            )
+            return redirect('perfil:criar')
+
+        # it has no cart
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Carrinho vazio'
+            )
+            return redirect('produto:lista')
+
+        # Gets variacao ids ['5', '2'] that are keys in carrinho
         carrinho = self.request.session.get('carrinho')
         carrinho_variacao_ids = [v for v in carrinho]
-
+        
+        # Variacao in db [<Variacao: variacao aaaaaa>, <Variacao: variação b>]
         bd_variacoes = list(
+            # 3 queries produto_variacao, produto_produto, produto_produto
+            # Variacao.objects.filter(id__in=carrinho_variacao_ids)
+
+            # 1 query produto_variacao (debug_toolbar)
             Variacao.objects.select_related('produto').filter(
                 id__in=carrinho_variacao_ids
             )
         )
 
         for variacao in bd_variacoes:
-            vid = str(variacao.id)
+            # variacao {'id': 1, 'produto_id': 1, 'nome': 'variacao aaaaaa',
+            #  'preco': 123123.0, 'preco_promocional': 123.0, 'estoque': 1}
+        
+            vid = str(variacao.id) # int
             estoque = variacao.estoque
 
+            # carrinho values
             qtd_carrinho = carrinho[vid]['quantidade']
             preco_unt = carrinho[vid]['preco_unitario']
             preco_unt_promo = carrinho[vid]['preco_unitario_promocional']
-        
+
+            # warning not active
             warning_msg_estoque = ''
 
+            # admin had decreased the stock and product was already on cart
             if estoque < qtd_carrinho:
+                # decreases quantidade on cart of a VID, 4 the max on estoque
                 carrinho[vid]['quantidade'] = estoque
 
+                # adjusts all already sum prices, 4 the new quantidade
                 carrinho[vid]['preco_quantitativo'] = estoque * preco_unt
-                carrinho[vid]['preco_quantitativo_promocional'] = estoque * preco_unt_promo
+                carrinho[vid]['preco_quantitativo_promocional'] = estoque * \
+                    preco_unt_promo
 
-                warning_msg_estoque = 'warning sla'
+                # TRIGGER UP the warning
+                warning_msg_estoque = 'Estoque insuficiente para alguns produtos do seu carrinho. '\
+                    'Reduzimos a quantidade desses produtos. Por Favor, '\
+                    'verifique quais produtos foram afetados, a seguir:'
 
+            # if estoque < qtd_carrinho, it falls here
             if warning_msg_estoque:
                 messages.warning(
                     self.request,
@@ -54,44 +85,51 @@ class SalvarPedido(View):
                 )
                 self.request.session.save()
                 return redirect('produto:carrinho')
+        
+        # passes through utils and get sum of it
+        qtd_total_carrinho = utils.cart_total_qtd(carrinho) # variation_id 
+        valor_total_carrinho = utils.cart_totals(carrinho) # all prices
 
-            qtd_total_carrinho = utils.cart_total_qtd(carrinho)
-            valor_total_carrinho = utils.cart_totals(carrinho)
+        # register on db pedido_pedido:
+        # id| total | status | usuario_id | qtd_total
+        pedido = Pedido(
+            usuario=self.request.user,
+            total=valor_total_carrinho,
+            qtd_total=qtd_total_carrinho,
+            status='C',
+        )
+        pedido.save()
 
-            pedido = Pedido(
-                usuario=self.request.user,
-                total=valor_total_carrinho,
-                qtd_total=qtd_total_carrinho,
-                status='C'
+        # register on db all those values in  pedido_itempedido
+        # maybe because of pedido=pedido right below
+        ItemPedido.objects.bulk_create(
+            [
+                ItemPedido(
+                    pedido=pedido,
+                    produto=v['produto_nome'],
+                    produto_id=v['produto_id'],
+                    variacao=v['variacao_nome'],
+                    variacao_id=v['variacao_id'],
+                    preco=v['preco_quantitativo'],
+                    preco_promocional=v['preco_quantitativo_promocional'],
+                    quantidade=v['quantidade'],
+                    imagem=v['imagem'],
+                
+                # carrinho.values() get those v[''] values
+                ) for v in carrinho.values() 
+            ]
+        )
+
+        # del the no needed cart
+        del self.request.session['carrinho']
+
+        # redirects to pedido/pagar/pk
+        return redirect(
+            reverse(
+                'pedido:pagar',
+                kwargs={'pk': pedido.pk}
             )
-            pedido.save()
-
-            ItemPedido.objects.bulk_create(
-                [
-                    ItemPedido(
-                        pedido=pedido,
-                        produto=v['produto_nome'],
-                        produto_id=v['produto_id'],
-                        variacao=v['variacao_nome'],
-                        variacao_id=v['variacao_id'],
-                        preco=v['preco_quantitativo'],
-                        preco=v['preco_quantitativo_promocional'],
-                        quantidade=v['quantidade'],
-                        imagem=v['imagem'],
-
-                    ) for v in carrinho.values()
-                ]
-            )
-            
-            del self.request.session['carrinho']
-
-            return redirect(
-                reverse(
-                    'pedido:pagar',
-                    kwargs={'pk': pedido.pk}
-                )
-            )
-
+        )
 
 
 class Detalhe(View):
