ec336ae3477094e8b66c12d1a741320df1a63629
imHireki <imhireki@gmail.com>
2021-05-26 14:17:37 -0300

    fix navbar

--- produto/templatetags/hkfilters.py
+++ produto/templatetags/hkfilters.py
@@ -11,3 +11,8 @@ def formata_preco(val):
 @register.filter
 def get_total(carrinho) -> str:
     return utils.get_total(carrinho)
+
+@register.filter
+def get_qtd_total(carrinho):
+    return utils.get_qtd_total(carrinho)
+    
